6e4f2876167ecad1d791b888698ad39d73e8cd3e
wangtnq <wangtnq@gmail.com>
2020-04-08 13:43:11 +0700

    system time

--- src/ngonngulaptrinhjava/thuchanh/timgiatritrongmang.java
+++ src/ngonngulaptrinhjava/thuchanh/timgiatritrongmang.java
@@ -5,20 +5,9 @@ import java.util.Scanner;
 public class timgiatritrongmang {
     public static void main(String[] args) {
 
-        String[] students = {"anh", "an", "hung"};
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("student's Name: ");
-        String input_name = scanner.nextLine();
-        boolean isExist = false;
-        for (int i = 0; i < students.length; i++) {
-            if (students[i].equals(input_name)) {
-                System.out.println("Position of the students in the list " + input_name + " is: " + (i + 1));
-                isExist = true;
-                break;
-            }
-        }
-        if (!isExist) {
-            System.out.println("Not found " + input_name + " in the list.");
+        for(int i=1; i<=5; i++){
+            if(i==1) break;
+            System.out.println("Hello");
         }
 
     }
