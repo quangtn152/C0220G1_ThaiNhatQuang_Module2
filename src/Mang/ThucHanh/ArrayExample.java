9c758efe5a42f6ac707dd27216d07fa879724528
wangtnq <wangtnq@gmail.com>
2020-04-09 14:47:12 +0700

    su dung try..catch

---
+++ src/Mang/ThucHanh/ArrayExample.java
@@ -0,0 +1,16 @@
+package Mang.ThucHanh;
+
+import java.util.Random;
+
+public class ArrayExample {
+    public Integer[] createdRandom() {
+        Random rd = new Random();
+        Integer[] arr = new Integer[100];
+        System.out.println("Danh sach Mang: ");
+        for (int i = 0; i < 100; i++) {
+            arr[i] = rd.nextInt(100);
+            System.out.print(arr[i] + " ");
+        }
+        return arr;
+    }
+}
