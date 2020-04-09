3ec913d0bfff1d26ac69e5ea5a2fbad141224b53
wangtnq <wangtnq@gmail.com>
2020-04-09 14:23:48 +0700

    update

--- src/Mang/ThucHanh/TimGiaTriNhoNhat.java
+++ src/Mang/ThucHanh/TimGiaTriNhoNhat.java
@@ -1,13 +1,13 @@
 package Mang.ThucHanh;
 
 public class TimGiaTriNhoNhat {
-    public static int minValue(int []array) {
+    public static int minValue(int[] array) {
         int min = array[0];
-        int index =0;
-        for (int i= 1;i<array.length;i++){
-            if(array[i]<min){
-                min=array[i];
-                index=i;
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                index = i;
             }
 
         }
@@ -15,8 +15,8 @@ public class TimGiaTriNhoNhat {
     }
 
     public static void main(String[] args) {
-        int []arr={2,4,5,2,1,3,6};
-        int index=minValue(arr);
-        System.out.println("Gia tri nho nhat cua mang: "+ arr[index]);
+        int[] arr = {2, 4, 5, 2, 1, 3, 6};
+        int index = minValue(arr);
+        System.out.println("Gia tri nho nhat cua mang: " + arr[index]);
     }
 }
