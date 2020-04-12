39d9ee276fa12effdf3baca19ecf180521dd2a6a
wangtnq <wangtnq@gmail.com>
2020-04-12 17:20:31 +0700

    su dung lop IllegalTriangleException

---
+++ src/Mang/BaiTap/IllegalTriangleException.java
@@ -0,0 +1,46 @@
+package Mang.BaiTap;
+
+public class IllegalTriangleException extends Exception {
+
+    public IllegalTriangleException(String s) {
+        super(s);
+    }
+//    private double side1,side2,side3;
+
+//    public IllegalTriangleException(double side1, double side2, double side3) {
+//        this.setSide1(side1);
+//        this.setSide2(side2);
+//        this.setSide3(side3);
+//        if ((side1+ side2)<side3 || (side1+ side3)<side2 || (side2+ side3)<side1){
+//            message();
+//        }
+//
+//    }
+//    private void message() {
+//        System.out.println("IllegalTriangleException");
+//    }
+
+//    public double getSide1() {
+//        return side1;
+//    }
+//
+//    public void setSide1(double side1) {
+//        this.side1 = side1;
+//    }
+//
+//    public double getSide2() {
+//        return side2;
+//    }
+//
+//    public void setSide2(double side2) {
+//        this.side2 = side2;
+//    }
+//
+//    public double getSide3() {
+//        return side3;
+//    }
+//
+//    public void setSide3(double side3) {
+//        this.side3 = side3;
+//    }
+}
