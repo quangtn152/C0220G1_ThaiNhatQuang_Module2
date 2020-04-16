95e4d1df5111828fa2b6c2c2f0a14b237195455f
wangtnq <wangtnq@gmail.com>
2020-04-16 09:25:36 +0700

    update

---
+++ src/P1_NgonNguLaptrinhJava/thuchanh/UocSoChungLonNhat.java
@@ -0,0 +1,26 @@
+package P1_NgonNguLaptrinhJava.thuchanh;
+
+import java.util.Scanner;
+
+public class UocSoChungLonNhat {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner nhap = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        a = nhap.nextInt();
+        System.out.println("Enter b: ");
+        b = nhap.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 || b == 0)
+            System.out.println("No greatest common factor");
+        while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+        System.out.println("Greatest common factor: " + a);
+
+    }
+}
