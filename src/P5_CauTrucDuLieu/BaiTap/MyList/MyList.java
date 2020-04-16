15d583aa3d1166515e78217eca8d7d849184a131
wangtnq <wangtnq@gmail.com>
2020-04-16 16:05:35 +0700

    MyList

--- src/P5_CauTrucDuLieu/BaiTap/MyList/MyList.java
+++ src/P5_CauTrucDuLieu/BaiTap/MyList/MyList.java
@@ -1,4 +1,98 @@
 package P5_CauTrucDuLieu.BaiTap.MyList;
 
-public class MyList {
+import java.util.Arrays;
+
+public class MyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elements;
+
+    public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyList(int capacity) {
+        elements=new Object[capacity];
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+
+    public void ensureCapa(int minCapa) {
+        int newSize = elements.length + minCapa;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    public void add(int index, E e) {
+        if (size == elements.length) {
+            ensureCapa(1);
+        }
+        for (int i = elements.length - 1; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = e;
+        size++;
+    }
+
+    public E remove(int index) {
+        Object a = elements[index];
+        for (int i = index; i < elements.length - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        size--;
+        return (E) a;
+    }
+
+    public E get(int i) {
+        if (i > size || i < 0) {
+            throw new IndexOutOfBoundsException("Index: " + i + " size: " + size);
+        }
+        return (E) elements[i];
+    }
+
+    public MyList<E> clone() {
+        MyList<E> cloneList = new MyList<>();
+        cloneList.elements = Arrays.copyOf(elements, size);
+        cloneList.size = size;
+
+        return cloneList;
+
+    }
+
+    public boolean contains(E o) {
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == o) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int indexOf(E o) {
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == o) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean add(E e) {
+        if (size == elements.length) {
+            ensureCapa(1);
+        }
+        elements[size++] = e;
+        return true;
+    }
+
+
+    public void clear() {
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
 }
--- src/P5_CauTrucDuLieu/BaiTap/MyList/MyListTest.java
+++ src/P5_CauTrucDuLieu/BaiTap/MyList/MyListTest.java
@@ -1,4 +1,45 @@
 package P5_CauTrucDuLieu.BaiTap.MyList;
 
 public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> myList = new MyList<>();
+        myList.add(0);
+        myList.add(1);
+        myList.add(2);
+        System.out.println("MyList:");
+
+        for (int i = 0; i < myList.getSize(); i++) {
+            System.out.print(myList.get(i));
+        }
+        System.out.println("\nAdd: 5 ");
+        myList.add(1, 5);
+//        System.out.println(myList.getSize());
+        for (int i = 0; i < myList.getSize(); i++) {
+            System.out.print(myList.get(i));
+        }
+        System.out.println("\nRemove: 5 ");
+        myList.remove(1);
+        for (int i = 0; i < myList.getSize(); i++) {
+            System.out.print(myList.get(i));
+        }
+        System.out.println("\nClone: ");
+        MyList<Integer> myList2 = myList.clone();
+        for (int i = 0; i < myList2.getSize(); i++) {
+            System.out.println("mylist2 " + i + " : " + myList2.get(i));
+        }
+        System.out.println("\nContains: ");
+        if (myList.contains(2)) {
+            System.out.println("Mylist chua 2");
+        } else System.out.println("Mylist chua khong chua 2");
+
+        System.out.println("Vi tri cua 1: " + myList.indexOf(1));
+
+        System.out.println("\nClear:");
+        myList.clear();
+        System.out.println("size:" + myList.getSize());
+
+        for (int i = 0; i < myList.getSize(); i++) {
+            System.out.println("mylist " + i + " : " + myList.get(i));
+        }
+    }
 }
